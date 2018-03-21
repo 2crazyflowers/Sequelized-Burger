8ab757a7835d79eadfccd501fdd2adfbef34e7a0
Sara <bracewell.sara@gmail.com>
2018-03-20 22:16:16 -0500

    updated files so devoured and delete work and update in both mysql and on browser

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -9,58 +9,71 @@ module.exports = function(app) {
   // Create all our routes and set up logic within those routes where required.
   
   // Index Page (render all burgers to browser)
+  //had to change to "/index" to get this to work properly
+  //Get route to get all the burgers
   app.get("/index", function(req, res) {
 
     db.Burger.findAll({}).then(function(dbBurger) {
       // We have access to the Burgers as an argument inside of the callback function
-      //create handlebars object
+      //have to create handlebars object in order for the html page to load anything other than a json array
       var hbsObject = {
         burgers: dbBurger
       };
 
       console.log(hbsObject);
+      //must render here with "index" as part of the call and the handlebars object as the secondary item
       res.render("index", hbsObject);
 
     });
   });
 
+  //POST route to create a burger
   app.post("/api/burgers", function(req, res) {
 
     db.Burger.create({
       burger_name: req.body.burger_name,
-      devoured: req.body.devoured
+      // devoured: req.body.devoured
 
     }).then(function(dbBurger) {
     // We have access to the new Burger as an argument inside of the callback function
       console.log(dbBurger);
+
+      //for some reason this does not have to be a res.redirect to get it to refresh the page with the correct information
       res.json(dbBurger);
 
     });
   });
 
-  app.put("/api/burgers", function(req, res) {
+//PUT route to update or change status on devour
+  app.put("/api/burgers/:id", function(req, res) {
 
     db.Burger.update({
-        burger_name: req.body.burger_name,
-        devoured: req.body.devoured
+        devoured: true
       }, {
         where: {
-          id: req.body.id
+          id: req.params.id
         }
       }).then(function(dbBurger) {
         //console.log(dbBurger);
-        res.redirect("/index");
+        //must use redirect here to get to update page with new information (if I was allowing user to do that)
+        //res.redirect("/index");
+        //res.json(dbBurger);
+        res.end();
+
       });
   });
 
-
+//DELETE route to toss the wrapper
   app.delete("/api/burgers/:id", function(req, res) {
     db.Burger.destroy({
         where: {
           id: req.params.id
         }
       }).then(function(dbBurger) {
-        res.redirect("/index");
+        //must use redirect here to get to destroy page with new information
+        //res.redirect("/index");
+        
+
       });
   });
 
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,26 +2,31 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
   $(".change-devour").on("click", function(event) {
+    event.preventDefault();
+
     var id = $(this).data("id");
-    var newDevour = $(this).data("newdevour");
+    // var newDevour = $(this).data("newdevour");
 
-    var newDevourState = {
-      devoured: newDevour
-    };
+    // var newDevourState = {
+    //   devoured: newDevour
+    // };
 
-    // Send the PUT request which updates the database
+    // Send the PUT request which updates the database with change in devour
     $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: newDevourState
+      method: "PUT",
+      data: {
+        devoured: true
+      }
     }).then(
       function() {
-        console.log("changed devour to", newDevour);
+        console.log("changed devour to true");
+        
         // Reload the page to get the updated list
         location.reload();
       }
     );
   });
-
+//takes user create burger and adds it to the database with a default devoured state of 0 or false
   $(".create-form").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
@@ -31,9 +36,9 @@ $(function() {
       devoured: 0
     };
 
-    // Send the POST request.
+    // Send the POST request for the new burger
     $.ajax("/api/burgers", {
-      type: "POST",
+      method: "POST",
       data: newBurger
     }).then(
       function() {
@@ -49,8 +54,8 @@ $(function() {
 
     // Send the DELETE request.
     $.ajax("/api/burgers/" + id, {
-      type: "DELETE",
-    }).then(
+      method: "DELETE",
+    }).then( 
       function() {
         console.log("deleted burger", id);
         // Reload the page to get the updated list
@@ -58,12 +63,21 @@ $(function() {
       }
     );
   });
+
+  // This function grabs todos from the database and updates the view
+  // function getBurgers() {
+  //   $.get("/api/burgers", function(data) {
+  //     burgers = data;
+  //     // Reload the page to get the updated list
+  //     location.reload();
+  //   });
+  // }
 });
 
 //testing to see if code is being registered
-function listening() {
-  console.log("javascript burgers.js is listening");
-};
+// function listening() {
+//   console.log("javascript burgers.js is listening");
+// };
 
-listening();
+// listening();
 
